bda02ea1a809b4306f5dfa741e90a5787b4d34c4
roopagayatri <roopagayatrit14@gmail.com>
2025-11-23 00:35:32 -0600

    Added Angular export demo route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ExportDemoComponent } from './components/export-demo/export-demo.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'export-demo', component: ExportDemoComponent },
   { path: '**', redirectTo: '' }
 ];
